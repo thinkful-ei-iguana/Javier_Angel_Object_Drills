fa014838174b28992709a0ef9961b2babb6f13cd
Angel Alicea <ala31996@gmail.com>
2019-10-23 12:00:38 -0400

    finished object creator drill

---
+++ Drills.js
@@ -0,0 +1,13 @@
+'use strict';
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+      return 'Hello!';
+    }
+  };
+}
+let newObj2 = createMyObject();
+console.log(newObj2.sayHello());
