e9fc798396d53b242e7bf2ead1ecfd516c33193b
Angel Alicea <ala31996@gmail.com>
2019-10-23 12:26:17 -0400

    completed person maker drill

--- Drills.js
+++ Drills.js
@@ -21,3 +21,14 @@ function updateObject(obj) {
 
   return obj;
 }
+
+function personMaker(){
+    var person = {
+        firstName: 'Paul',
+        lastName: 'Jones',
+        fullName: function(){
+            return `${this.firstName} ${this.lastName}`
+        } 
+    };
+    return person;
+}
